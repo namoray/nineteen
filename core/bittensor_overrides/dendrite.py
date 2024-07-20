db611637a35070d489cbb1e735e68f88670b6bbb
namoray <neuvaamor@proton.me>
2024-07-20 14:45:24 +0100

    think i fixed el dendrite

--- core/bittensor_overrides/dendrite.py
+++ core/bittensor_overrides/dendrite.py
@@ -1,30 +1,73 @@
 import asyncio
+import os
 import time
-from typing import Any, AsyncGenerator, List, Union
+from typing import Any, AsyncGenerator, Union
+import uuid
 
 import aiohttp
+import httpx
+from pydantic import BaseModel
 from core import bittensor_overrides as bt
 
 
+class SigningResponse(BaseModel):
+    signature: str
+
+
 class dendrite:
-    def _log_outgoing_request(self, *args) -> None:
-        """I don't like their logging, it says success regardless of a success x)"""
-        ...
+    def __init__(self):
+        # Initialize the parent class
+        super(dendrite, self).__init__()
+
+        # Unique identifier for the instance
+        self.uuid = str(uuid.uuid1())
+
+        # Get the external IP
+        self.external_ip = bt.networking.get_external_ip()  # noqa
+
+        self.synapse_history: list = []
+
+        self._session: aiohttp.ClientSession | None = None
+        self._httpx_client: httpx.AsyncClient | None = None
+        self._signing_headers = {
+            "Authorization": os.getenv("SIGNING_API_KEY"),
+        }
+
+    @property
+    async def session(self) -> aiohttp.ClientSession:
+        if self._session is None:
+            self._session = aiohttp.ClientSession()
+        return self._session
+
+    @property
+    async def httpx_client(self) -> httpx.AsyncClient:
+        if self._httpx_client is None:
+            self._httpx_client = httpx.AsyncClient(timeout=10)
+        return self._httpx_client
+
+    async def _sign_mesage(self, message: str):
+        response = await (await self.httpx_client).post(
+            "https://signing-service.onrender.com/endpoints/sign_payload",
+            json={"message": message},
+            headers=self._signing_headers,
+        )
+
+        return response.json()["signature"]
+
+    async def close_session(self):
+        if self._session:
+            await self._session.close()
+            self._session = None
+        if self._httpx_client:
+            await self._httpx_client.aclose()
+            self._httpx_client = None
+
+    async def aclose_session(self):
+        if self._session:
+            await self._session.close()
+            self._session = None
 
     def _get_endpoint_url(self, target_axon: bt.axon, request_name: str) -> str:
-        """
-        Constructs the endpoint URL for a network request to a target axon.
-
-        This internal method generates the full HTTP URL for sending a request to the specified axon. The
-        URL includes the IP address and port of the target axon, along with the specific request name.
-
-        Args:
-            target_axon: The target axon object containing IP and port information.
-            request_name: The specific name of the request being made.
-
-        Returns:
-            str: A string representing the complete HTTP URL for the request.
-        """
         endpoint = f"{target_axon.ip}:{str(target_axon.port)}"
 
         # TODO: COMMENT OUT FOR MAINNET
@@ -36,125 +79,29 @@ class dendrite:
 
         return f"http://{endpoint}/{request_name}"
 
-    async def forward(
+    def _handle_request_errors(
         self,
-        axons: Union[
-            List[Union[bt.AxonInfo, bt.axon]],
-            Union[bt.AxonInfo, bt.axon],
-        ],
-        synapse: bt.Synapse = bt.Synapse(),
-        connect_timeout: float = 1.5,
-        response_timeout: float = 3,
-        deserialize: bool = True,
-        run_async: bool = True,
-        streaming: bool = False,
-        log_requests_and_responses: bool = True,
-    ) -> List[Union[AsyncGenerator[Any, Any], bt.Synapse, bt.StreamingSynapse]]:
-        """
-        Asynchronously sends requests to one or multiple Axons and collates their responses.
-
-        This function acts as a bridge for sending multiple requests concurrently or sequentially
-        based on the provided parameters. It checks the type of the target Axons, preprocesses
-        the requests, and then sends them off. After getting the responses, it processes and
-        collates them into a unified format.
-
-        When querying an Axon that sends back data in chunks using the Dendrite, this function
-        returns an AsyncGenerator that yields each chunk as it is received. The generator can be
-        iterated over to process each chunk individually.
-
-        For example:
-            >>> ...
-            >>> dendrte = bt.dendrite(wallet = wallet)
-            >>> async for chunk in dendrite.forward(axons, synapse, timeout, deserialize, run_async, streaming):
-            >>>     # Process each chunk here
-            >>>     print(chunk)
-
-        Args:
-            axons (Union[List[Union['bt.AxonInfo', 'bt.axon']], Union['bt.AxonInfo', 'bt.axon']]):
-                The target Axons to send requests to. Can be a single Axon or a list of Axons.
-            synapse (bt.Synapse, optional): The Synapse object encapsulating the data. Defaults to a new bt.Synapse instance.
-            timeout (float, optional): Maximum duration to wait for a response from an Axon in seconds. Defaults to 12.0.
-            deserialize (bool, optional): Determines if the received response should be deserialized. Defaults to True.
-            run_async (bool, optional): If True, sends requests concurrently. Otherwise, sends requests sequentially. Defaults to True.
-            streaming (bool, optional): Indicates if the response is expected to be in streaming format. Defaults to False.
-
-        Returns:
-            Union[AsyncGenerator, bt.Synapse, List[bt.Synapse]]: If a single Axon is targeted, returns its response.
-            If multiple Axons are targeted, returns a list of their responses.
-        """
-        is_list = True
-        # If a single axon is provided, wrap it in a list for uniform processing
-        if not isinstance(axons, list):
-            is_list = False
-            axons = [axons]
-
-        # Check if synapse is an instance of the StreamingSynapse class or if streaming flag is set.
-        is_streaming_subclass = issubclass(synapse.__class__, bt.StreamingSynapse)
-        if streaming != is_streaming_subclass:
-            bt.logging.warning(
-                f"Argument streaming is {streaming} while issubclass(synapse, StreamingSynapse) is {synapse.__class__.__name__}. This may cause unexpected behavior."
+        synapse: bt.Synapse,
+        request_name: str,
+        exception: Exception,
+        connection_timeout: float,
+        response_timeout: float,
+    ) -> None:
+        if isinstance(exception, aiohttp.ClientConnectorError):
+            synapse.dendrite.status_code = "503"
+            synapse.dendrite.status_message = (
+                f"Service at {synapse.axon.ip}:{str(synapse.axon.port)}/{request_name} unavailable."
             )
-        streaming = is_streaming_subclass or streaming
-
-        async def query_all_axons(
-            is_stream: bool,
-        ) -> Any:
-            """
-            Handles requests for all axons, either in streaming or non-streaming mode.
-
-            Args:
-                is_stream: If True, handles the axons in streaming mode.
-
-            Returns:
-                List of Synapse objects with responses.
-            """
-
-            async def single_axon_response(
-                target_axon: Union[bt.AxonInfo, bt.axon],
-            ) -> AsyncGenerator[Any, Any] | bt.Synapse | bt.StreamingSynapse:
-                """
-                Retrieve response for a single axon, either in streaming or non-streaming mode.
-
-                Args:
-                    target_axon: The target axon to send request to.
-
-                Returns:
-                    A Synapse object with the response.
-                """
-                if is_stream:
-                    # If in streaming mode, return the async_generator
-                    return self.call_stream(
-                        target_axon=target_axon,
-                        synapse=synapse.copy(),
-                        connect_timeout=connect_timeout,
-                        response_timeout=response_timeout,
-                        deserialize=deserialize,
-                        log_requests_and_responses=log_requests_and_responses,
-                    )
-                else:
-                    # If not in streaming mode, simply call the axon and get the response.
-                    return await self.call(
-                        target_axon=target_axon,
-                        synapse=synapse.copy(),
-                        connect_timeout=connect_timeout,
-                        response_timeout=response_timeout,
-                        deserialize=deserialize,
-                        log_requests_and_responses=log_requests_and_responses,
-                    )
-
-            # If run_async flag is False, get responses one by one.
-            if not run_async:
-                return [await single_axon_response(target_axon) for target_axon in axons]
-            # If run_async flag is True, get responses concurrently using asyncio.gather().
-            return await asyncio.gather(*(single_axon_response(target_axon) for target_axon in axons))
-
-        # Get responses for all axons.
-        responses = await query_all_axons(streaming)
-        # Return the single response if only one axon was targeted, else return all responses
-        if len(responses) == 1 and not is_list:
-            return responses[0]  # type: ignore
+        elif isinstance(exception, asyncio.TimeoutError):
+            if "Connection timeout" in str(exception):
+                synapse.dendrite.status_code = "408"
+                synapse.dendrite.status_message = f"Initial connection timeout after {connection_timeout} seconds."
+            else:
+                synapse.dendrite.status_code = "408"
+                synapse.dendrite.status_message = f"Response timeout after {response_timeout} seconds."
         else:
-            return responses  # type: ignore
+            synapse.dendrite.status_code = "422"
+            synapse.dendrite.status_message = f"Failed to parse response object with error: {str(exception)}"
 
     async def call_stream(
         self,
@@ -173,15 +120,6 @@ class dendrite:
         useful for processing large responses piece by piece without waiting for the entire
         data to be transmitted.
 
-        Args:
-            target_axon (Union['bt.AxonInfo', 'bt.axon']): The target Axon to send the request to.
-            synapse (bt.Synapse, optional): The Synapse object encapsulating the data. Defaults to a new bt.Synapse instance.
-            timeout (float, optional): Maximum duration to wait for a response (or a chunk of the response) from the Axon in seconds. Defaults to 12.0.
-            deserialize (bool, optional): Determines if each received chunk should be deserialized. Defaults to True.
-
-        Yields:
-            object: Each yielded object contains a chunk of the arbitrary response data from the Axon.
-            bt.Synapse: After the AsyncGenerator has been exhausted, yields the final filled Synapse.
         """
 
         # Record start time
@@ -283,10 +221,6 @@ class dendrite:
         timeout_settings = aiohttp.ClientTimeout(sock_connect=connect_timeout, sock_read=response_timeout)
 
         try:
-            # Log outgoing request
-            if log_requests_and_responses:
-                self._log_outgoing_request(synapse)
-
             # Make the HTTP POST request
             async with (await self.session).post(
                 url,
@@ -306,9 +240,6 @@ class dendrite:
             self._handle_request_errors(synapse, request_name, e, connect_timeout, response_timeout)
 
         finally:
-            if log_requests_and_responses:
-                self._log_incoming_response(synapse)
-
             # Log synapse event history
             self.synapse_history.append(bt.Synapse.from_headers(synapse.to_headers()))
 
@@ -317,30 +248,6 @@ class dendrite:
                 return synapse.deserialize()  # noqa: B012
             else:
                 return synapse
-
-    def _handle_request_errors(
-        self,
-        synapse: bt.Synapse,
-        request_name: str,
-        exception: Exception,
-        connection_timeout: float,
-        response_timeout: float,
-    ) -> None:
-        if isinstance(exception, aiohttp.ClientConnectorError):
-            synapse.dendrite.status_code = "503"
-            synapse.dendrite.status_message = (
-                f"Service at {synapse.axon.ip}:{str(synapse.axon.port)}/{request_name} unavailable."
-            )
-        elif isinstance(exception, asyncio.TimeoutError):
-            if "Connection timeout" in str(exception):
-                synapse.dendrite.status_code = "408"
-                synapse.dendrite.status_message = f"Initial connection timeout after {connection_timeout} seconds."
-            else:
-                synapse.dendrite.status_code = "408"
-                synapse.dendrite.status_message = f"Response timeout after {response_timeout} seconds."
-        else:
-            synapse.dendrite.status_code = "422"
-            synapse.dendrite.status_message = f"Failed to parse response object with error: {str(exception)}"
 
     async def preprocess_synapse_for_request(
         self,
@@ -514,4 +421,4 @@ class dendrite:
             # ... some operations ...
             del dendrite  # This will implicitly invoke the __del__ method and close the session.
         """
-        self.close_session()
+        asyncio.run(self.close_session())
