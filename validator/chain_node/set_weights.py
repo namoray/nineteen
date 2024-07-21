1b95d566c01878c3877642a2cfe08eb164273cbf
namoray <neuvaamor@proton.me>
2024-07-21 20:51:00 +0100

    set weights

--- validator/chain_node/set_weights.py
+++ validator/chain_node/set_weights.py
@@ -2,14 +2,16 @@ import asyncio
 from dataclasses import asdict
 import json
 import os
+import time
 from redis.asyncio import Redis
 from redis import Redis as SyncRedis
-
+import numpy as np
 from validator.utils import redis_constants as rcst
 from validator.chain_node.keypair import RedisGappedKeypair
 from core.logging import get_logger
 import bittensor as bt
 from validator.utils import redis_dataclasses as rdc
+from bittensor.utils import weight_utils
 
 logger = get_logger(__name__)
 
@@ -23,24 +25,67 @@ async def get_public_keypair_info(redis_db: Redis) -> rdc.PublicKeypairInfo:
     return rdc.PublicKeypairInfo(**json.loads(info))
 
 
-async def poll_for_weights_then_set(redis_db: Redis, wallet: bt.wallet, subtensor: bt.subtensor) -> None:
+MAX_ATTEMPTS = 10
+TIME_TO_SLEEP_BETWEEN_ATTEMPTS = 10
+
+
+def _repeatedly_try_to_seight_weights(
+    weights_to_set: rdc.WeightsToSet, wallet: bt.wallet, metagraph: bt.metagraph, subtensor: bt.subtensor
+) -> None:
+    success = False
+    attempts = 0
+
+    logger.info(
+        f"Will try to set weights for a maximum of {MAX_ATTEMPTS} times and {TIME_TO_SLEEP_BETWEEN_ATTEMPTS * (MAX_ATTEMPTS) * (MAX_ATTEMPTS -1 ) / 2} seconds"
+    )
+    while not success and attempts < 10:
+        weights_array = np.zeros_like(metagraph.uids, dtype=np.float32)
+        for uid, weight in zip(weights_to_set.uids, weights_to_set.values):
+            weights_array[uid] = weight
+
+        logger.info(f"weights_array: {weights_array}, metagraph uids: {metagraph.uids}")
+        (
+            processed_weight_uids,
+            processed_weights,
+        ) = weight_utils.process_weights_for_netuid(
+            uids=metagraph.uids,
+            weights=weights_array,
+            netuid=weights_to_set.netuid,
+            subtensor=subtensor,
+            metagraph=metagraph,
+        )
+
+        logger.info(f"Setting weights: {processed_weights} for uids: {processed_weight_uids}....")
+
+        success, message = subtensor.set_weights(
+            wallet=wallet,
+            uids=processed_weight_uids,
+            weights=processed_weights,
+            version_key=weights_to_set.version_key,
+            netuid=weights_to_set.netuid,
+            wait_for_finalization=True,
+            wait_for_inclusion=True,
+        )
+
+        if not success:
+            attempts += 1
+            logger.error(f"Set weights success: {success}.\t Message: {message}")
+            logger.info(f"Retrying in {attempts * 10} seconds...")
+            time.sleep(attempts * 10)
+
+
+async def poll_for_weights_then_set(
+    redis_db: Redis, wallet: bt.wallet, metagraph: bt.metagraph, subtensor: bt.subtensor
+) -> None:
     while True:
         logger.info("Polling redis for weights to set...")
         _, payload_raw = await redis_db.blpop(rcst.WEIGHTS_TO_SET_QUEUE_KEY)
 
-        payload = rdc.WeightsToSet(**json.loads(payload_raw))
+        weights_to_set = rdc.WeightsToSet(**json.loads(payload_raw))
 
-        logger.info(f"Setting weights, on netuid: {payload.netuid}")
+        logger.info(f"Setting weights, on netuid: {weights_to_set.netuid}")
 
-        subtensor.set_weights(
-            wallet=wallet,
-            uids=payload.uids,
-            weights=payload.values,
-            version_key=payload.version_key,
-            netuid=payload.netuid,
-            wait_for_finalization=True,
-            wait_for_inclusion=True,
-        )
+        _repeatedly_try_to_seight_weights(weights_to_set, wallet, metagraph, subtensor)
 
 
 async def main():
@@ -58,6 +103,7 @@ async def main():
     wallet._hotkey = keypair
 
     subtensor = bt.subtensor(network=os.getenv("SUBTENSOR_NETWORK", "finney"))
+    metagraph = subtensor.metagraph(netuid=os.getenv("NETUID", 176))
 
     # TODO: Change back to 19
     netuid = os.getenv("NETUID", 176)
@@ -65,13 +111,15 @@ async def main():
     test_env = os.getenv("ENV", "prod").lower() == "test"
 
     if test_env:
-        payload = json.dumps(asdict(rdc.WeightsToSet(uids=[0, 1, 2], values=[1, 1, 0], version_key=100, netuid=netuid)))
+        payload = json.dumps(
+            asdict(rdc.WeightsToSet(uids=[0, 1, 2], values=[0.3, 0.01, 0.5], version_key=10000000000, netuid=netuid))
+        )
         await redis_db.rpush(
             rcst.WEIGHTS_TO_SET_QUEUE_KEY,
             payload,
         )
 
-    await poll_for_weights_then_set(redis_db, wallet, subtensor)
+    await poll_for_weights_then_set(redis_db, wallet, metagraph, subtensor)
 
 
 if __name__ == "__main__":
--- validator/signing_service/utils.py
+++ validator/signing_service/utils.py
@@ -4,8 +4,6 @@ import os
 import sys
 from validator.signing_service import constants as cst
 from substrateinterface import Keypair
-import random
-import time
 
 ANSI_COLOR_CODES = {
     "$BLUE": "\033[34m",
@@ -53,77 +51,6 @@ def get_logger(name: str) -> logging.Logger:
 
 
 logger = get_logger(__name__)
-
-
-def retry(
-    f,
-    logger,
-    exceptions=Exception,
-    tries=-1,
-    delay=0,
-    max_delay=None,
-    backoff=1,
-    jitter=0,
-):
-    """
-    Executes a function and retries it if it failed.
-
-    :param f: the function to execute.
-    :param exceptions: an exception or a tuple of exceptions to catch. default: Exception.
-    :param tries: the maximum number of attempts. default: -1 (infinite).
-    :param delay: initial delay between attempts. default: 0.
-    :param max_delay: the maximum value of delay. default: None (no limit).
-    :param backoff: multiplier applied to delay between attempts. default: 1 (no backoff).
-    :param jitter: extra seconds added to delay between attempts. default: 0.
-                   fixed if a number, random if a range tuple (min, max)
-    :param logger: logger.warning(fmt, error, delay) will be called on failed attempts.
-                   default: retry.logging_logger. if None, logging is disabled.
-    :returns: the result of the f function.
-    """
-    _tries, _delay = tries, delay
-    while _tries:
-        try:
-            return f()
-        except exceptions as e:
-            _tries -= 1
-            if not _tries:
-                raise
-
-            if logger is not None:
-                logger.warning("%s, retrying in %s seconds...", e, _delay)
-
-            time.sleep(_delay)
-            _delay *= backoff
-
-            if isinstance(jitter, tuple):
-                _delay += random.uniform(*jitter)
-            else:
-                _delay += jitter
-
-            if max_delay is not None:
-                _delay = min(_delay, max_delay)
-
-
-def format_weights_error_message(error_message: dict) -> str:
-    """
-    Formats an error message from the Subtensor error information to using in extrinsics.
-
-    Args:
-        error_message (dict): A dictionary containing the error information from Subtensor.
-
-    Returns:
-        str: A formatted error message string.
-    """
-    err_type = "UnknownType"
-    err_name = "UnknownError"
-    err_description = "Unknown Description"
-
-    if isinstance(error_message, dict):
-        err_type = error_message.get("type", err_type)
-        err_name = error_message.get("name", err_name)
-        err_docs = error_message.get("docs", [])
-        err_description = err_docs[0] if len(err_docs) > 0 else err_description
-    return f"Subtensor returned `{err_name} ({err_type})` error. This means: `{err_description}`"
 
 
 def construct_wallet_path(wallet_name: str, hotkey_name: str) -> str:
