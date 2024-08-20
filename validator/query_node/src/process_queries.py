1df435d884c4ed4eec5a76f3fb48512bb5ede25b
namoray <neuvaamor@proton.me>
2024-08-20 11:23:52 +0100

    non stream on its way

--- validator/query_node/src/process_queries.py
+++ validator/query_node/src/process_queries.py
@@ -1,6 +1,7 @@
 import asyncio
 import json
 from redis.asyncio import Redis
+from validator.models import Contender
 from validator.query_node.src.query_config import Config
 from core.tasks import Task
 from core import tasks_config as tcfg
@@ -21,6 +22,49 @@ async def _decrement_requests_remaining(redis_db: Redis, task: Task):
     await redis_db.decr(key)
 
 
+async def _handle_stream_query(
+    config: Config, message: rdc.QueryQueueMessage, contenders_to_query: list[Contender]
+) -> bool:
+    success = False
+    for contender in contenders_to_query:
+        node = await get_node(config.psql_db, contender.node_id, config.netuid)
+        generator = await query.query_node_stream(
+            config=config, contender=contender, payload=message.query_payload, node=node
+        )
+
+        await query.consume_generator(
+            config=config,
+            generator=generator,
+            job_id=message.job_id,
+            synthetic_query=message.query_type == "synthetic",
+            contender=contender,
+            node=node,
+            payload=message.query_payload,
+        )
+        if generator is None:
+            continue
+        else:
+            success = True
+
+    return success
+
+
+async def _handle_nonstream_query(
+    config: Config, message: rdc.QueryQueueMessage, contenders_to_query: list[Contender]
+) -> bool:
+    success = False
+    for contender in contenders_to_query:
+        node = await get_node(config.psql_db, contender.node_id, config.netuid)
+        generator = await query.query_node_stream(
+            config=config, contender=contender, payload=message.query_payload, node=node
+        )
+
+
+            success = True
+
+    return success
+
+
 async def process_task(config: Config, message: rdc.QueryQueueMessage):
     task = Task(message.task)
 
@@ -31,35 +75,16 @@ async def process_task(config: Config, message: rdc.QueryQueueMessage):
 
     stream = tcfg.TASK_TO_CONFIG[task].is_stream
 
-    if stream:
-        # Decide which contenders to query.
-        async with await config.psql_db.connection() as connection:
-            contenders_to_query = await get_contenders_for_task(connection, task)
-        
-        if contenders_to_query is None:
-            raise ValueError("No contenders to query! :(")
-        # Query em
-        for contender in contenders_to_query:
-            node = await get_node(config.psql_db, contender.node_id, config.netuid)
-            generator = await query.query_node_stream(
-                config=config, contender=contender, payload=message.query_payload, node=node
-            )
-                
+    async with await config.psql_db.connection() as connection:
+        contenders_to_query = await get_contenders_for_task(connection, task)
 
-            await query.consume_generator(
-                config=config,
-                generator=generator,
-                job_id=message.job_id,
-                synthetic_query=message.query_type == "synthetic",
-                contender=contender,
-                node=node,
-                payload=message.query_payload,
-            )
-            if generator is None:
-                continue
+    if contenders_to_query is None:
+        raise ValueError("No contenders to query! :(")
+
+    if stream:
+        return _handle_stream_query(config, message, contenders_to_query)
     else:
-        # Handle non-stream tasks here
-        pass
+        return _handle_nonstream_query(config, message, contenders_to_query)
 
 
 async def listen_for_tasks(config: Config):
