d1ba25f2442bcb7d33722192fb7683abc50601dc
namoray <neuvaamor@proton.me>
2024-09-22 20:18:54 +0100

    Post to 19

---
+++ validator/utils/post/nineteen.py
@@ -0,0 +1,134 @@
+import enum
+import json
+import time
+from typing import Any, Dict, List, Optional, Union
+import httpx
+from pydantic import BaseModel
+from fiber.logging_utils import get_logger
+from fiber import Keypair
+
+from validator.models import RewardData
+
+logger = get_logger(__name__)
+
+
+class DataTypeToPost(enum.Enum):
+    REWARD_DATA = 1
+    UID_RECORD = 2
+    MINER_CAPACITIES = 3
+    VALIDATOR_INFO = 4
+
+
+# TODO: Change
+BASE_URL = "https://dev.tauvision.ai/"
+
+data_type_to_url = {
+    DataTypeToPost.REWARD_DATA: BASE_URL + "v1/store/reward_data",
+    DataTypeToPost.UID_RECORD: BASE_URL + "v1/store/uid_records",
+    DataTypeToPost.MINER_CAPACITIES: BASE_URL + "v1/store/miner_capacities",
+    DataTypeToPost.VALIDATOR_INFO: BASE_URL + "v1/store/validator_info",
+}
+
+# Turn off if you don't wanna post your validator info to nineteen.ai
+POST_TO_NINETEEN_AI = True
+
+
+def _sign_timestamp(keypair: Keypair, timestamp: float) -> str:
+    return f"0x{keypair.sign(str(timestamp)).hex()}"
+
+
+async def post_to_nineteen_ai(
+    data_to_post: Union[Dict[str, Any], List[Dict[str, Any]]],
+    keypair: Keypair,
+    data_type_to_post: DataTypeToPost,
+    timeout: int = 10,
+) -> None:
+    if not POST_TO_NINETEEN_AI:
+        return
+    timestamp = time.time()
+    public_address = keypair.ss58_address
+    signed_timestamp = _sign_timestamp(keypair, timestamp)
+
+    headers = {
+        "Content-Type": "application/json",
+        "x-timestamp": str(timestamp),
+        "x-signature": signed_timestamp,
+        "x-public-key": public_address,
+    }
+
+    async with httpx.AsyncClient(timeout=timeout) as client:
+        try:
+            resp = await client.post(
+                url=data_type_to_url[data_type_to_post],
+                data=json.dumps(data_to_post),
+                headers=headers,
+            )
+            logger.debug(f"Resp status code from tauvision: {resp.status_code} for post type {data_type_to_post}")
+            return resp
+        except Exception as e:
+            logger.error(f"Error when posting to tauvision to store data for {data_type_to_post}: {repr(e)}")
+
+
+class RewardDataPostBody(RewardData):
+    testnet: bool
+
+
+class ValidatorInfoPostBody(BaseModel):
+    versions: str
+    validator_hotkey: str
+
+
+class MinerCapacitiesPostObject(BaseModel):
+    validator_hotkey: str
+    miner_hotkey: str
+    task: str
+    volume: float
+
+
+class MinerCapacitiesPostBody(BaseModel):
+    data: List[MinerCapacitiesPostObject]
+
+    def dump(self):
+        return [
+            {
+                "validator_hotkey": ob.validator_hotkey,
+                "miner_hotkey": ob.miner_hotkey,
+                "task": ob.task.value,
+                "volume": ob.volume,
+            }
+            for ob in self.data
+        ]
+
+
+class UidRecordPostObject(BaseModel):
+    axon_uid: int
+    miner_hotkey: str
+    validator_hotkey: str
+    task: str
+    declared_volume: float
+    consumed_volume: float
+    total_requests_made: int
+    requests_429: int
+    requests_500: int
+    period_score: Optional[float]
+
+    def dict(self):
+        return {
+            "axon_uid": self.axon_uid,
+            "miner_hotkey": self.miner_hotkey,
+            "validator_hotkey": self.validator_hotkey,
+            "task": self.task,
+            "declared_volume": self.declared_volume,
+            "consumed_volume": self.consumed_volume,
+            "total_requests_made": self.total_requests_made,
+            "requests_429": self.requests_429,
+            "requests_500": self.requests_500,
+            "period_score": self.period_score,
+        }
+
+
+class UidRecordsPostBody(BaseModel):
+    data: List[UidRecordPostObject]
+
+    def dump(self):
+        return [ob.dict() for ob in self.data]
