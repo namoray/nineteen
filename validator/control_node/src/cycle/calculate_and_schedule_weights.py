11baeb2d83459f4e6dc513e99accb8cd7ce8841f
namoray <neuvaamor@proton.me>
2024-09-23 12:44:00 +0100

    set weights to use the metagraph if we have no data

--- validator/control_node/src/cycle/calculate_and_schedule_weights.py
+++ validator/control_node/src/cycle/calculate_and_schedule_weights.py
@@ -5,6 +5,7 @@ Calculates and schedules weights every SCORING_PERIOD
 from dotenv import load_dotenv
 import os
 
+
 load_dotenv(os.getenv("ENV_FILE", ".vali.env"))
 
 import asyncio
@@ -17,6 +18,8 @@ from fiber.chain import weights
 from fiber.logging_utils import get_logger
 from core import constants as ccst
 from validator.db.src.sql.nodes import get_vali_node_id
+from fiber.chain import fetch_nodes
+from fiber.chain.models import Node
 
 logger = get_logger(__name__)
 
@@ -79,7 +82,34 @@ async def main():
     logger.debug(f"Config: {config}")
     await config.psql_db.connect()
 
-    await get_and_set_weights(config)
+    success = await get_and_set_weights(config)
+    if not success:
+        logger.error("Failed to set weights using db values :(")
+
+    logger.warning(
+        "\n\n!!!! Setting weights using the metagraph only since we have no data on the miners in the db. Please cancel if you do not want to do this !!! \n\n"
+    )
+    await asyncio.sleep(10)
+    nodes: list[Node] = fetch_nodes.get_nodes_for_netuid(config.substrate, config.netuid)
+    node_ids = [node.node_id for node in nodes]
+    node_weights = [node.incentive for node in nodes]
+    validator_node_id = await get_vali_node_id(config.psql_db, config.netuid)
+    if validator_node_id is None:
+        raise ValueError("Validator node id not found")
+
+    await asyncio.to_thread(
+        weights.set_node_weights,
+        substrate=config.substrate,
+        keypair=config.keypair,
+        node_ids=node_ids,
+        node_weights=node_weights,
+        netuid=config.netuid,
+        version_key=ccst.VERSION_KEY,
+        validator_node_id=int(validator_node_id),
+        wait_for_inclusion=True,
+        wait_for_finalization=True,
+        max_attempts=3,
+    )
 
 
 if __name__ == "__main__":
