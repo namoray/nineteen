7a486f17111b27c24c02f454aa13d712bf82e6ff
namoray <neuvaamor@proton.me>
2024-09-23 14:12:46 +0100

    migrate

--- migrate.sh
+++ migrate.sh
@@ -5,5 +5,5 @@ cd nineteen
 sudo -E ./bootstrap.sh
 source $HOME/.bashrc
 pm2 delete all
-cat ~/vision/.default.env  # Or however you want to view your config
+cat ~/vision/.default.env  # Or however you want to view your old config
 python core/create_config.py
