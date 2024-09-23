b9616bb0b0d3e040fd82b89f7a9d990a37421b3c
namoray <152790419+namoray@users.noreply.github.com>
2024-09-23 14:51:00 +0100

    Organic server port (#19)

--- utils/launch_validator.sh
+++ utils/launch_validator.sh
@@ -1,1 +1,13 @@
-docker compose --env-file .vali.env -f docker-compose.yml up -d --build
+#!/bin/bash
+
+set -a
+source .vali.env
+set +a
+
+if [ -n "$ORGANIC_SERVER_PORT" ]; then
+  echo "ORGANIC_SERVER_PORT is set to '$ORGANIC_SERVER_PORT'. Starting entry_node service."
+  docker compose --env-file .vali.env -f docker-compose.yml --profile entry_node_profile up -d --build
+else
+  echo "ORGANIC_SERVER_PORT is not set. Starting without entry_node service."
+  docker compose --env-file .vali.env -f docker-compose.yml up -d --build
+fi
