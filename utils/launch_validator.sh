f05b4c11faa5f3bdbc7431ae5f3adccd3fb8ca63
namoray <neuvaamor@proton.me>
2024-09-23 14:47:57 +0100

    docker compose no dash

--- utils/launch_validator.sh
+++ utils/launch_validator.sh
@@ -6,8 +6,8 @@ set +a
 
 if [ -n "$ORGANIC_SERVER_PORT" ]; then
   echo "ORGANIC_SERVER_PORT is set to '$ORGANIC_SERVER_PORT'. Starting entry_node service."
-  docker-compose --env-file .vali.env -f docker-compose.yml --profile entry_node_profile up -d --build
+  docker compose --env-file .vali.env -f docker-compose.yml --profile entry_node_profile up -d --build
 else
   echo "ORGANIC_SERVER_PORT is not set. Starting without entry_node service."
-  docker-compose --env-file .vali.env -f docker-compose.yml up -d --build
+  docker compose --env-file .vali.env -f docker-compose.yml up -d --build
 fi
