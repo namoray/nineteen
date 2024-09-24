f308f7d598b7c6b13bcc7c8b3bb9e79624c4583f
namoray <neuvaamor@proton.me>
2024-09-24 12:45:39 +0100

    fiber logs

--- validator/utils/substrate/query_substrate.py
+++ validator/utils/substrate/query_substrate.py
@@ -1,7 +1,8 @@
 from typing import Any
 from substrateinterface import SubstrateInterface
-from loguru import logger
+from fiber.logging_utils import get_logger
 
+logger = get_logger(__name__)
 
 def query_substrate(substrate: SubstrateInterface, module: str, method: str, params: list[Any], return_value: bool = True) -> Any:
     try:
