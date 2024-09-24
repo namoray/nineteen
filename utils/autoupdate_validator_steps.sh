83877609c920d55c53a8dfb27e0eac1751841624
namoray <neuvaamor@proton.me>
2024-09-24 13:15:47 +0100

    update steps

--- utils/autoupdate_validator_steps.sh
+++ utils/autoupdate_validator_steps.sh
@@ -3,6 +3,5 @@
 # THIS FILE CONTAINS THE STEPS NEEDED TO AUTOMATICALLY UPDATE THE REPO ON A TAG CHANGE
 # THIS FILE ITSELF MAY CHANGE FROM UPDATE TO UPDATE, SO WE CAN DYNAMICALLY FIX ANY ISSUES
 
-docker compose --env-file .vali.env -f docker-compose.yml  build
 ./utils/launch_validator.sh
 echo "Autoupdate steps complete :)"
--- utils/update_validator_steps.sh
+++ utils/update_validator_steps.sh
@@ -3,7 +3,5 @@
 # THIS FILE CONTAINS THE STEPS NEEDED TO MANUALLY UPDATE THE REPO ON A TAG CHANGE
 # THIS FILE ITSELF MAY CHANGE FROM UPDATE TO UPDATE, SO WE CAN DYNAMICALLY FIX ANY ISSUES
 
-docker compose --env-file .vali.env -f docker-compose.yml  build
-docker compose --env-file .vali.env -f docker-compose.yml run -e LOCALHOST=false --entrypoint "python /validator/utils/contender/nuke_contender_history.py" control_node
 ./utils/launch_validator.sh
 echo "Update steps complete :)"
