7f7a846931b23c3ade4959c396fd8deac606c996
namoray <neuvaamor@proton.me>
2024-09-28 15:25:29 +0100

    sd

--- utils/launch_validator.sh
+++ utils/launch_validator.sh
@@ -4,7 +4,8 @@ set -a
 source .vali.env
 set +a
 
-if [ -n "$ORGANIC_SERVER_PORT" ]; then
+# Check if ORGANIC_SERVER_PORT is set to 'none' - for legacy config reasons
+if [ -n "$ORGANIC_SERVER_PORT" ] && [ "${ORGANIC_SERVER_PORT,,}" != "none" ]; then
   echo "ORGANIC_SERVER_PORT is set to '$ORGANIC_SERVER_PORT'. Starting entry_node service."
   docker compose --env-file .vali.env -f docker-compose.yml --profile entry_node_profile up -d --build --remove-orphans
 else
