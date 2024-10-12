bb342aec268d05c7033868d4bbb44dd0b8e4e18b
Arpan Tripathi <tripathiarpan20@gmail.com>
2024-10-12 13:20:16 +0100

    Update refresh_contenders.py

--- validator/control_node/src/cycle/refresh_contenders.py
+++ validator/control_node/src/cycle/refresh_contenders.py
@@ -202,7 +202,7 @@ async def _post_contender_stats_to_nineteen(config: Config):
                 miner_hotkey=contender.node_hotkey,
                 validator_hotkey=config.keypair.ss58_address,
                 task=contender.task,
-                volume=contender.capacity_to_score,
+                volume=contender.capacity,
             ).model_dump(mode="json")
         )
     await post_to_nineteen_ai(
