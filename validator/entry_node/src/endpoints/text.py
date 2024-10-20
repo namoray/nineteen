114b855c810ca4912536cb322737e8d9ae536da6
namoray <152790419+namoray@users.noreply.github.com>
2024-10-20 13:56:26 +0100

    Revert "retry submissions" (#78)
    This reverts commit bbe13231c4046a826738f68b208d374a7a7d98ec.

--- validator/entry_node/src/endpoints/text.py
+++ validator/entry_node/src/endpoints/text.py
@@ -12,7 +12,6 @@ from validator.entry_node.src.core.middleware import verify_api_key_rate_limit
 from validator.utils.redis import redis_constants as rcst
 from validator.utils.generic import generic_constants as gcst
 from validator.entry_node.src.models import request_models
-from validator.entry_node import constants as ecst
 import asyncio
 from validator.utils.query.query_utils import load_sse_jsons
 from redis.asyncio.client import PubSub
@@ -64,15 +63,6 @@ async def _get_first_chunk(pubsub: PubSub, job_id: str) -> str | None:
     return None
 
 
-async def _confirm_job_submission(pubsub: PubSub, redis_db: Redis, job_id: str, organic_message: str) -> bool:
-    await pubsub.subscribe(f"{gcst.ACKNLOWEDGED}:{job_id}")
-    await redis_db.lpush(rcst.QUERY_QUEUE_KEY, organic_message)
-    try:
-        return await asyncio.wait_for(_wait_for_acknowledgement(pubsub, job_id), timeout=1)
-    except asyncio.TimeoutError:
-        return False
-
-
 async def make_stream_organic_query(
     redis_db: Redis,
     payload: dict[str, Any],
@@ -82,18 +72,17 @@ async def make_stream_organic_query(
     organic_message = _construct_organic_message(payload=payload, job_id=job_id, task=task)
 
     pubsub = redis_db.pubsub()
-    attempts_to_confirm = 0
-    while attempts_to_confirm < ecst.MAX_ATTEMPTS_TO_CONFIRM_JOB_SUBMISSION:
-        if await _confirm_job_submission(pubsub, redis_db, job_id, organic_message):
-            break
-        attempts_to_confirm += 1
-    if attempts_to_confirm == ecst.MAX_ATTEMPTS_TO_CONFIRM_JOB_SUBMISSION:
-        logger.error(
-            f"❌ Failed to confirm job submission for job {job_id} within {ecst.MAX_ATTEMPTS_TO_CONFIRM_JOB_SUBMISSION} attempts. Task: {task}"
-        )
-        raise HTTPException(status_code=500, detail="Unable to process request - please try again later.")
+    await pubsub.subscribe(f"{gcst.ACKNLOWEDGED}:{job_id}")
+    await redis_db.lpush(rcst.QUERY_QUEUE_KEY, organic_message)  # type: ignore
 
     first_chunk = None
+    try:
+        await asyncio.wait_for(_wait_for_acknowledgement(pubsub, job_id), timeout=1)
+    except asyncio.TimeoutError:
+        logger.error(
+            f"Query node down? No confirmation received for job {job_id} within timeout period. Task: {task}, model: {payload['model']}"
+        )
+        raise HTTPException(status_code=500, detail="Unable to process request")
 
     await pubsub.subscribe(f"{rcst.JOB_RESULTS}:{job_id}")
     logger.info("Here waiting for a message!")
