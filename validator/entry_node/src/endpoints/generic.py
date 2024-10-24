fac36af204e6a251ac065e0c5ae3031895316983
Arpan Tripathi <tripathiarpan20@gmail.com>
2024-10-24 11:49:03 +0100

    Update generic.py

--- validator/entry_node/src/endpoints/generic.py
+++ validator/entry_node/src/endpoints/generic.py
@@ -2,12 +2,13 @@ from typing import Any
 from fiber.logging_utils import get_logger
 from fastapi.routing import APIRouter
 from validator.entry_node.src.models.text_models import llm_models
+from core import task_config as tcfg
 
 logger = get_logger(__name__)
 
 
 async def models() -> list[dict[str, Any]]:
-    return llm_models
+    return tcfg.get_public_task_configs()
 
 
 router = APIRouter()
